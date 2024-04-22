38501eaae18039c493fa25b677179c6620fbc944
HwiJoon Lee <107892477+Halfgear@users.noreply.github.com>
2024-04-22 00:30:05 -0400

    added q4 analysis

--- DataCleaning/main.py
+++ DataCleaning/main.py
@@ -6,6 +6,7 @@ data.fillna('no', inplace=True)
 
 noValues = ["no", "No", "N/A", "NA", "NaN", "n/a", "None", "none", "none used for course"]
 
+
 def clean_f(item):
     if item in noValues:
         return "Did not use"
@@ -31,24 +32,29 @@ def clean_hrrange(item):
         return 30
     if item in "40-50 hours":
         return 45
+    if item in "15-20":
+        return 18
+    if item in "12+":
+        return 12
     return item
 
 
 data["What kind of AI Model did you use?"] = data["What kind of AI Model did you use?"].apply(clean_f)
 
 data.loc[data["Was AI available when you took OOD?"] == "No", "What kind of AI Model did you use?"] = "Did not use"
-data.loc[data["Was AI available when you took OOD?"] == "No", "How much have you used AI in OOD, if at all?"] = 0
-data.loc[data["Was AI available when you took OOD?"] == "No", ("If you are using or used AI, do you believe that it "
-                                                               "has benefited or is benefiting your academic "
-                                                               "performance? ")] = "Did not use"
+data.loc[data["Was AI available when you took OOD?"] == "No", (f"How much have you used / are "
+                                                               "you using AI in OOD if at all?\n")] = 0
+data.loc[data["Was AI available when you took OOD?"] == "No", (f"If you use/used AI, do you believe that it"
+                                                               f" benefits/benefitted your academic performance? ")]\
+    = "Did not use"
 data.loc[data["Was AI available when you took OOD?"] == "No", ("How do you use AI to assist your work if you use them?"
                                                                " (Select one that you use the most)")] = "Did not use"
 
-data["If you are using or used AI, do you believe that it has benefited or is benefiting your academic performance? "] \
-    = data[("If you are using or used AI, do you believe that it "
-            "has benefited or is benefiting your academic performance? ")].apply(clean_no)
-data["What was/is your grade from OOD? (0-100)"] = (data["What was/is your grade from OOD? (0-100)"]
-                                                       .apply(clean_grade))
+data["If you use/used AI, do you believe that it benefits/benefitted your academic performance? "] \
+    = (data[f"If you use/used AI, do you believe that it benefits/benefitted your academic performance? "]
+       .apply(clean_no))
+data[f"What was/is your grade from OOD? (0-100)"] = \
+    (data[f"What was/is your grade from OOD? (0-100)"].apply(clean_grade))
 data["How many hours did you spend on OOD per week?"] = (data["How many hours did you spend on OOD per week?"]
                                                          .apply(clean_hrrange))
 
